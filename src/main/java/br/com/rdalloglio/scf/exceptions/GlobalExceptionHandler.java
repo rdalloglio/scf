d2d82136a73c3567d802e6e564c5729ea6f63d60
Rafael Dall'Oglio <rafael.dalloglio@gmail.com>
2025-05-05 18:10:55 -0300

    erro para tipo invalido

---
+++ src/main/java/br/com/rdalloglio/scf/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,46 @@
+package br.com.rdalloglio.scf.exceptions;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+
+import br.com.rdalloglio.scf.enums.CategoryType;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+            errors.put(error.getField(), error.getDefaultMessage())
+        );
+        return errors;
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleInvalidEnum(HttpMessageNotReadableException ex) {
+        Map<String, String> error = new HashMap<>();
+        if (ex.getCause() instanceof InvalidFormatException formatException) {
+            String fieldName = formatException.getPath().get(0).getFieldName();
+            String targetType = formatException.getTargetType().getSimpleName();
+            String value = formatException.getValue().toString();
+            error.put(fieldName, "Valor inválido: '" + value + "'. Valores aceitos para " + fieldName + ": " +
+                    Arrays.toString(CategoryType.values()));
+        } else {
+            error.put("erro", "Requisição inválida");
+        }
+        return error;
+    }
+}
