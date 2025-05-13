7d6899ddf25ae1328cb95c758c4774f56e393d86
Rafael Dall'Oglio <rafael.dalloglio@gmail.com>
2025-05-13 14:14:20 -0300

    aumentando força bcrypt e readme

--- src/main/java/br/com/rdalloglio/scf/security/SecurityConfig.java
+++ src/main/java/br/com/rdalloglio/scf/security/SecurityConfig.java
@@ -52,6 +52,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(12);
     }
 }
